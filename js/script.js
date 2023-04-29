33e779234c966f2020b92a44125340f6d13ea29f
luca betti sorbelli <luca betti sorbelli>
2023-04-29 12:20:18 +0200

    5 numeri casuali

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
+
 // visualizzare in pagina 5 numeri casuali
 
+let arrayNumeri = [];
+
+function numeriRandom (max, min) {
+
+    for (let i = 0; i < 5; i++) {
+
+        let risultato = Math.floor(Math.random() * (max - min + 1) + min);
+
+        arrayNumeri.push(risultato);
+    }
+}
+
+numeriRandom( 100, 1);
+console.log(arrayNumeri);
+
+let listaArray = document.querySelector(".array");
+listaArray.innerText = arrayNumeri;
 // timer 30 secondi poi scompaiono
 
 // prompt, uno alla volta, numeri visualizzati
